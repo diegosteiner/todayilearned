2c608dd736923fd18c3c333c248cdd1537c5e199
Diego Steiner <diego.steiner@renuo.ch>
2016-03-14 09:51:40 +0100

    blureffect

--- app/assets/javascripts/til.js
+++ app/assets/javascripts/til.js
@@ -1,20 +1,29 @@
 $(function(){
 
-	$(document.body).on("click", "#flash p", function(e) {
-		e.preventDefault();
-		$(this).fadeOut(200);
-	});
+  $(document.body).on("click", "#flash p", function(e) {
+    e.preventDefault();
+    $(this).fadeOut(200);
+  });
 
-	$(".site_nav__search, .site_nav__about").on("click", ".site_nav__link", function(e) {
-		e.preventDefault();
-		$(this).closest('li')
-			.toggleClass('site_nav--open')
-			.find(":input:visible").eq(0).focus();
-	});
+  $(".site_nav__search, .site_nav__about").on("click", ".site_nav__link", function(e) {
+    e.preventDefault();
+    $(this).closest('li')
+      .toggleClass('site_nav--open')
+      .find(":input:visible").eq(0).focus();
+  });
 
-	$(window).on('scroll', function () {
-		var pixs = $(document).scrollTop()
-			pixs = pixs / 5;
-		$(".site_head").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" })     
-	});
+  $(window).on('scroll', function () {
+    var element = $(".site_head");
+    var pixs = $(document).scrollTop() / 5;
+
+    if(pixs == 0) {
+      element.css({"-webkit-filter": "blur(0)","filter": "blur(0)" });
+      element.css({"opacity":1});
+    } else if(pixs < 200) {
+      element.css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" });
+      element.css({"opacity":4/pixs});
+    } else if(element.css('opacity') > 0) {
+      element.css({"opacity":"0"});
+    }
+  });
 });
